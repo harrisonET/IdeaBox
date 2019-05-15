459c7324db5d685d536002e0d1e07399fb47872e
harrisonET <harrisoneliezer@gmail.com>
2019-05-15 11:52:10 +0800

    Version 2.0: new UI, storage using SQLite, Kanban Board Category

--- app/src/main/java/com/example/ideabox/AddActivity.java
+++ app/src/main/java/com/example/ideabox/AddActivity.java
@@ -1,54 +1,191 @@
 package com.example.ideabox;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class AddActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText ideaEditTxt;
     EditText descEditTxt;
+    Spinner spinner;
+    Spinner catSpinner;
+    LinearLayout mileLinear;
+    TextView displayCatView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        ideaEditTxt = findViewById(R.id.ideaEditTxt);
-        descEditTxt =  findViewById(R.id.descEditTxt);
+        displayCatView  = (TextView)findViewById(R.id.displayCatView);
+        ideaEditTxt = (EditText)findViewById(R.id.ideaEditTxt);
+        descEditTxt =   (EditText)findViewById(R.id.descEditTxt);
+
+        //milestone linearlayout
+        mileLinear = (LinearLayout) findViewById(R.id.milestoneLinear);
+
+        catSpinner = (Spinner) findViewById(R.id.catSpinner);
+        catSpinner.setOnItemSelectedListener(this);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> catAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinnerCat, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        catAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        // Apply the adapter to the spinner
+        catSpinner.setAdapter(catAdapter);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinnerArray, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
 
     public void addNew(View view){
         String title = ideaEditTxt.getText().toString();
         String desc = descEditTxt.getText().toString();
 
-        if (ideaChecker(title,desc) == true) {
-            SharedPreferences sharedPref = this.getSharedPreferences("com.example.ideabox", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putString(title, desc);
-            Toast.makeText(getApplicationContext(), "Idea called " + title + " is created!!", Toast.LENGTH_SHORT).show();
-            editor.commit();
+        List<Milestone> mList = new ArrayList<Milestone>();
 
+        //get milestone
+        Log.i("Tag for mileStone: ",Integer.toString(mileLinear.getChildCount()));
+        if (mileLinear.getChildCount() > 0){
+            for(int i = 0; i < mileLinear.getChildCount();i++){
+                EditText ed = (EditText)mileLinear.getChildAt(i);
+                mList.add(new Milestone(ed.getText().toString(),false));
+            }
+        }
+
+        String category;
+        //get get category
+        switch (displayCatView.getText().toString()){
+            case "Unlisted":category = "Unlisted";Log.i("Tag", "Unlisted Cat");break;
+            case "For Fun":category = "For Fun";Log.i("Tag", "For Fun Cat"); break;
+            case "Doing": category = "Doing";Log.i("Tag", "Doing Cat");break;
+            default:category = "Error";Log.i("Tag", "Default Cat");
+        }
+
+        if (ideaChecker(title,desc) == true) {
+            //insert into database sql
+            Idea idea = new Idea(title,desc,category,mList);
+            String sql = "INSERT INTO Idea (name,data) VALUES (?, ?)";
+            SQLiteStatement statement = MainActivity.eventsDB.compileStatement(sql);
+            try {
+                byte[] objByte = com.example.ideabox.ObjectSerializer.serialize(idea);
+                statement.bindString(1, idea.getName());
+                statement.bindBlob(2,objByte);
+                statement.execute();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            //displayDB();
+            Toast.makeText(getApplicationContext(), "Idea called " + title + " is created!!", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
-        ideaEditTxt.setText("");
-        descEditTxt.setText("");
     }
+
+    public void displayDB(){
+        Cursor c = MainActivity.eventsDB.rawQuery("SELECT * FROM Idea", null);
+        int nameIndex = c.getColumnIndex("name");
+        int ideaIndex = c.getColumnIndex("data");
+        int idIndex = c.getColumnIndex("id");
+        c.moveToFirst();
+        try {
+            while (c != null) {
+                Log.i("Name: ", c.getString(nameIndex));
+                Log.i("Data: ", com.example.ideabox.ObjectSerializer.deserialize(c.getBlob(ideaIndex)).toString());
+                Log.i("Id: ", Integer.toString(c.getInt(idIndex)));
+                c.moveToNext();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
     public boolean ideaChecker(String title, String desc){
         boolean ok = false;
-
+        String warning = "";
         if (title.isEmpty())
-            Toast.makeText(getApplicationContext(),"Cannot be empty!!!",Toast.LENGTH_LONG).show();
+            warning +=  "Title Or Description ";
         else
             ok = true;
 
+        //check for milestone
+        if (mileLinear.getChildCount() > 0){
+            for(int i = 0; i < mileLinear.getChildCount();i++){
+                EditText mile = (EditText)mileLinear.getChildAt(i);
+                if (mile.getText().toString().isEmpty()){
+                    if (warning.equals(""))
+                        warning += " One or more of the milestone";
+                    else {
+                        warning += ", One or more of the milestone";
+                    }
+                    ok = false;
+                    break;
+                }
+            }
+        }
+        if (!warning.isEmpty()) {
+            warning += " cannot be empty!";
+            Toast.makeText(getApplicationContext(), warning, Toast.LENGTH_LONG).show();
+
+        }
         return ok;
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("Test Spinner: ",parent.getItemAtPosition(position).toString() );
+        boolean f = parent.getItemAtPosition(position).toString().matches(".*\\d.*");
+        if (f){
+            mileLinear.removeAllViews();
+            int size = Integer.parseInt(parent.getItemAtPosition(position).toString());
+            generateMilestoneList(size);
+        }else{
+            displayCatView.setText(parent.getItemAtPosition(position).toString());
+            displayCatView.setTextSize(18);
+        }
+    }
+
+    public void generateMilestoneList(int size){
+        for (int i = 0; i < size; i++) {
+            // create a new textview
+            final EditText rowEditText = new EditText(this);
+            // set some properties of rowTextView or something
+            rowEditText.setTextSize(18);
+            rowEditText.setHint((i+1) + "" );
+            // add the textview to the linearlayout
+            mileLinear.addView(rowEditText);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
--- app/src/main/java/com/example/ideabox/MainActivity.java
+++ app/src/main/java/com/example/ideabox/MainActivity.java
@@ -5,8 +5,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,40 +21,144 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
+import java.util.ArrayList;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout linearLayout;
-    SharedPreferences sharedPreferences;
-    ScrollView scrollView;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    SectionsPageAdapter sectionsPageAdapter;
+    static Fragment unlistedF;
+    static Fragment forFunF;
+    static Fragment doingF;
+    static SQLiteDatabase eventsDB;
+    //idea list for each category
+    static ArrayList<Idea>uIdeaList;
+    static ArrayList<Idea>fIdeaList;
+    static ArrayList<Idea>dIdeaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
-        sharedPreferences = this.getSharedPreferences("com.example.ideabox", Context.MODE_PRIVATE);
-        scrollView = (ScrollView) findViewById(R.id.scroll);
-        loadIdeas();
+
+        uIdeaList = new ArrayList<Idea>();
+        fIdeaList = new ArrayList<Idea>();
+        dIdeaList = new ArrayList<Idea>();
+
+        sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        viewPager = (ViewPager) findViewById(R.id.container);
+        setUpViewPager(viewPager);
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        eventsDB = this.openOrCreateDatabase("IdeaDB", MODE_PRIVATE, null);
+        eventsDB.execSQL("CREATE TABLE IF NOT EXISTS Idea (name VARCHAR, data BLOB, id INTEGER PRIMARY KEY)");
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                Log.i("Page Selected: ", Integer.toString(i));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+        fillIdeaList();
+        //loadIdeas();
+    }
+
+    public void fillIdeaList(){
+        //displayDB();
+        loadDB();
+//        Log.i("U", uIdeaList.toString());
+//        Log.i("F", fIdeaList.toString());
+//        Log.i("D", dIdeaList.toString());
+
+    }
+
+    public void loadDB(){
+        clearIdeaLists();
+        Cursor c = eventsDB.rawQuery("SELECT * FROM Idea", null);
+        int nameIndex = c.getColumnIndex("name");
+        int ideaIndex = c.getColumnIndex("data");
+        int idIndex = c.getColumnIndex("id");
+        c.moveToFirst();
+        try {
+            while (c != null) {
+                //Log.i("Name: ", c.getString(nameIndex));
+                //Log.i("Data: ", com.example.ideabox.ObjectSerializer.deserialize(c.getBlob(ideaIndex)).toString());
+                //Log.i("Id: ", Integer.toString(c.getInt(idIndex)));
+                //put into ideaList
+                Idea idea = (Idea) com.example.ideabox.ObjectSerializer.deserialize(c.getBlob(ideaIndex));
+                if (idea.getCategory().equals("Unlisted")){
+                    uIdeaList.add(idea);
+                }else if (idea.getCategory().equals("For Fun"))
+                    fIdeaList.add(idea);
+                else if (idea.getCategory().equals("Doing")){
+                    dIdeaList.add(idea);
+                }else{
+                    Log.i("Anomaly", "Detected");
+                }
+                c.moveToNext();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void displayDB(){
+        Cursor c = eventsDB.rawQuery("SELECT * FROM Idea", null);
+        int nameIndex = c.getColumnIndex("name");
+        int ideaIndex = c.getColumnIndex("data");
+        int idIndex = c.getColumnIndex("id");
+        c.moveToFirst();
+        try {
+            while (c != null) {
+                Log.i("Name: ", c.getString(nameIndex));
+                Log.i("Data: ", com.example.ideabox.ObjectSerializer.deserialize(c.getBlob(ideaIndex)).toString());
+                Log.i("Id: ", Integer.toString(c.getInt(idIndex)));
+
+                if (c != null)
+                    c.moveToNext();
+                else
+                    break;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void clearIdeaLists(){
+        uIdeaList = new ArrayList<Idea>();
+        fIdeaList = new ArrayList<Idea>();
+        dIdeaList = new ArrayList<Idea>();
     }
 
     public void loadIdeas(){
-        int counter = 0;
-        Map<String,?> keys = sharedPreferences.getAll();
-        if (keys.size() != 0) {
-            for (Map.Entry<String, ?> entry : keys.entrySet()) {
-                Log.i("map values", entry.getKey() + ": " +
-                        entry.getValue().toString());
-                TextView textView = (TextView) linearLayout.getChildAt(counter);
-                textView.setText(entry.getKey());
-                textView.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.edittext_border));
-                counter++;
-            }
-        }
+
     }
+
+    private void setUpViewPager(ViewPager viewPager){
+        unlistedF = new UnlistedFragment();
+        forFunF = new ForFunFragment();
+        doingF = new DoingFragment();
+
+        sectionsPageAdapter.addFragment(unlistedF, "Unlisted");
+        sectionsPageAdapter.addFragment(forFunF, "For Fun");
+        sectionsPageAdapter.addFragment(doingF, "Doing");
+        viewPager.setAdapter(sectionsPageAdapter);
+    }
+
 
     public void goToEditView(View view){
         TextView textView = (TextView) view;
@@ -63,25 +172,6 @@ public class MainActivity extends AppCompatActivity {
     public void addIdeas(View view){
         Intent intent = new Intent(this, AddActivity.class);
         startActivity(intent);
-    }
-
-    public void eraseAll(View view){
-        new AlertDialog.Builder(this)
-                .setTitle("Erase All")
-                .setMessage("Do you want to delete all of your ideas ?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.clear();
-                        editor.commit();
-                        linearLayout.removeAllViews();
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Log.i("Alert", "Cancelled");
-                    }
-                }).show();
     }
 
 }
